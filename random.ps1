f86c71948f5a87941001b9a78a01d331218b09af
Anders Raben Hansen <12665561+sn0wflake@users.noreply.github.com>
2022-09-15 19:38:19 +0200

    Update random.ps1

--- random.ps1
+++ random.ps1
@@ -1,19 +1,7 @@
-$max = 100000
-$hasher = [System.Security.Cryptography.HashAlgorithm]::Create('sha')
-
 while(1) {
-    $time = Get-Date -UFormat %s
-    if((Get-Date -UFormat %s) -ne $time) {
-        $x += [System.BitConverter]::ToString($hasher.ComputeHash([System.Text.Encoding]::UTF8.GetBytes((Get-Date -UFormat %s) + $x + $x.Length + $max + $time))) -replace '[a-zA-Z-]',''
-        ($x.substring($x.length - $max.ToString().Length) % $max) + 1
-
-        # Trunkate string as to not make memory fill up.
-        if($x.Length -gt $max) {
-            $x = $x.Substring($max)
-        }
-
-        # Append as much randomness as possible. Can be omitted for much better speed.
-        } else {
-        $x += [System.BitConverter]::ToString($hasher.ComputeHash([System.Text.Encoding]::UTF8.GetBytes((Get-Date -UFormat %s) + $x + $x.Length + $max)))
+    for($i = 0; $i -le ([double](Get-Date -UFormat %s) % [math]::PI -shl 2); $i++) {
+        [double]$seed = ($seed + [double](Get-Date -UFormat %s)) / [math]::PI / [math]::E
     }
+
+    [int]([Math]::Pow($seed, $i) * [math]::PI * [math]::E % 1000)
 }
