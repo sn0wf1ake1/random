201d46a90c3e383f303e7f0cb005b93f4c9b55ec
Anders Raben Hansen <12665561+sn0wflake@users.noreply.github.com>
2022-09-29 13:37:32 +0200

    Update random.ps1

--- random.ps1
+++ random.ps1
@@ -1,6 +1,6 @@
 while(1) {
     for($i = 0; $i -le ([double](Get-Date -UFormat %s) % [math]::PI -shl 3); $i++) {
-        [double]$seed = ($seed + [double](Get-Date -UFormat %s)) / [math]::PI / [math]::E
+        [double]$seed = ($seed += [double](Get-Date -UFormat %s)) / [math]::PI / [math]::E
     }
 
     [int]([string]($seed % ($i / [math]::PI))).substring(7,5)
