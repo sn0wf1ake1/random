c5d10dc7a6bc17fc5e8e6cd5b87453732b8eaa99
Anders Raben Hansen <12665561+sn0wflake@users.noreply.github.com>
2022-10-02 06:06:18 +0200

    Update random.ps1

--- random.ps1
+++ random.ps1
@@ -3,5 +3,5 @@ while(1) {
         [double]$seed += [double](Get-Date -UFormat %s) / [math]::PI / [math]::E
     }
 
-    [int]([string]($seed % ($i / [math]::PI))).substring(7,5)
+    [int]([string]($seed)).substring(7,5)
 }
